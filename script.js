5fa113f63a904d52fbf770f1ecda5bf4fc1da71e
lfpc23 <159497174+lfpc23@users.noreply.github.com>
2024-02-09 00:36:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('si-btn').addEventListener('click', function() {
+  document.getElementById('response').textContent = '¡Genial! ¡Feliz San Valentín!';
+});
+
+document.getElementById('no-btn').addEventListener('click', function() {
+  document.getElementById('response').textContent = 'Oh, bueno, quizás la próxima vez.';
+});
+
+
+
